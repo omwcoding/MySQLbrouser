25dc6fc145653efcf4b5832b1c34e79b065d6e07
Omar Balde <116875415+omwcoding@users.noreply.github.com>
2023-01-19 00:23:29 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,19 +3,24 @@ DROP DATABASE mnemosine_db;
 CREATE DATABASE mnemosine_db;
 USE mnemosine_db;
 
-CREATE TABLE ATTRIBUTO(
+CREATE TABLE attributo (
     id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
     descrizione TEXT,
+    valore_stringa VARCHAR(255),
+    valore_numero DECIMAL(10,2),
+    valore_bool TINYINT(1),
+    valore_data DATE,
+    tipo_attributo ENUM('stringa','numero','bool','data') NOT NULL,
     
     PRIMARY KEY (id)
 );
 
 CREATE TABLE prodotto (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    price DECIMAL(10,2),
+    nome VARCHAR(255) NOT NULL,
+    descrizione TEXT,
+    prezzo_finale DECIMAL(10,2),
     attributo_id INT,
 
     PRIMARY KEY (id),
