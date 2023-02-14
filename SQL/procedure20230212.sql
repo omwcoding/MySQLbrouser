826ab60f3fdf2cc4db0015116f7628da486c6b3a
Omar Balde <116875415+omwcoding@users.noreply.github.com>
2023-02-14 22:32:47 +0100

    omar u bomber

--- SQL/procedure20230212.sql
+++ SQL/procedure20230212.sql
@@ -1,4 +1,4 @@
--- Active: 1672329880844@@127.0.0.1@3306@budb01
+-- Active: 1676405941074@@db4free.net@3306@budb01
 USE budb01;
 DELIMITER $$
 DROP PROCEDURE IF EXISTS crea_prodotto;
@@ -31,38 +31,26 @@ DELIMITER ;
 -- Per la tabella valore potresti fare la stessa cosa verificando se esiste già un valore con lo stesso nome o valore_prezzo o valore_int o valore_bool o valore_data.
 
 DELIMITER $$
-CREATE PROCEDURE insert_valore (IN val_text 		VARCHAR(255),
-								   val_prezzo 		DECIMAL(10,2),
-								   val_int 			INT,
-								   val_bool 		TINYINT(1),
-								   val_data 		DATE)
+CREATE PROCEDURE insert_valore (IN valore_attributo VARCHAR(255), IN tipo VARCHAR(121))
 BEGIN
-    DECLARE cnt INT;
-    SELECT COUNT(*) INTO cnt FROM valore 
-		WHERE valore_text = val_text 
-			or valore_prezzo = val_prezzo 
-			or valore_int = val_int 
-			or valore_bool = val_bool 
-			or valore_data = val_data;
-    IF cnt = 0 THEN
-        INSERT INTO valore (valore_text,
-							valore_prezzo,
-							valore_int,
-							valore_bool,
-							valore_data) 
-        VALUES (val_text,
-				val_prezzo,
-                val_int,
-                val_bool,
-                val_data);
+    DECLARE valore_esistente VARCHAR(255);
+
+    SELECT valore_char INTO valore_esistente
+    FROM valore
+    WHERE valore_char = valore_attributo;
+
+    IF (valore_esistente IS NULL) THEN
+        INSERT INTO valore (valore_char, tipo_valore) VALUES (valore_attributo, tipo);
     ELSE
         SELECT 'Valore già esistente' AS message;
     END IF;
-END $$
+END$$
 DELIMITER ;
+
 -- Nota: potrebbe essere necessario modificare i nomi dei campi nella query SELECT per adattarsi alla tua tabella valore specifica.
 
 
 -- Associare un attributo ad un prodotto e un valore ad un attributo
 -- CONTROLLI VARI
 -- VISTA GET PRODOTTI CON LA LORO STRUTTURA
+
---
+++ SQL/test.sql
@@ -0,0 +1,4 @@
+-- Active: 1676405941074@@db4free.net@3306@budb01
+USE budb01;
+
+CALL insert_valore('5', 'number');
